a20a646b274d6c05a6cadf7103271d7e9ab4f6ea
federicolenzi <federicolenzi@gmail.com>
2010-10-24 17:49:10 +0000

    ultimo commit eaea

--- src/computacionEvolutiva/Main.py
+++ src/computacionEvolutiva/Main.py
@@ -5,24 +5,25 @@ from EvolutionaryAlgorithm import EvolutionaryAlgorithm
 from timeit import Timer
 
 if __name__ == '__main__':
-#    print len(sys.argv)
-#    if len(sys.argv) < 6:
-#        print "Faltan Parametros"
+    if len(sys.argv) < 6:
+        print "Faltan Parametros"
+        print "Uso:"
+        print "python Main.py <queens> <initialPopulation> <iterations> <mutationProbability> <numberOfIndividues>"
+        print "Main.py 10 50 5000 15 5"        
+    else:
+        queens = int(sys.argv[1])
+        initialPopulation = int(sys.argv[2])
+        iterations = int(sys.argv[3])
+        mutationProbability = int(sys.argv[4])
+        numberOfIndividues = int(sys.argv[5])
         
-
-    queens = 20
-    initialPopulation = 50
-    iterations = 5000
-    mutationProbability = 15
-    numberOfIndividues = 5
-    
-    logger=Logger.Logger(sys.stdout,'./salida.txt')
-    sys.stdout=logger
-    
-    evolutionaryAlgorithm = EvolutionaryAlgorithm(queens, initialPopulation, iterations, mutationProbability, numberOfIndividues) 
-    t = Timer(evolutionaryAlgorithm.runAlgorithm)
-    time = t.timeit(1)
-       
-    print "\nTiempo de ejecucion ", time, "segundos"
-    print "Numero de soluciones", len(evolutionaryAlgorithm.getSolutions())
-    logger.close()
+        logger=Logger.Logger(sys.stdout,'./salida.txt')
+        sys.stdout=logger
+        
+        evolutionaryAlgorithm = EvolutionaryAlgorithm(queens, initialPopulation, iterations, mutationProbability, numberOfIndividues) 
+        t = Timer(evolutionaryAlgorithm.runAlgorithm)
+        time = t.timeit(1)
+           
+        print "\nTiempo de ejecucion ", time, "segundos"
+        print "Numero de soluciones", len(evolutionaryAlgorithm.getSolutions())
+        logger.close()
